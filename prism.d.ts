693d4f5b60cd2573d15c494d18c5a045f91a5462
Azaken1248 <suvkrm@gmail.com>
2025-08-28 02:48:05 +0530

    Added A Module Declaration For Prism Core

---
+++ prism.d.ts
@@ -0,0 +1,7 @@
+declare module 'prismjs/components/prism-core' {
+  const highlight: any;
+  const languages: any;
+  export { highlight, languages };
+}
+
+declare module 'prismjs/components/prism-sql';
